a81342907d5269e90d3135879555338725a11266
bhavya691 <70103543+bhavya691@users.noreply.github.com>
2021-11-16 23:16:36 +0530

    Add files via upload

---
+++ c-106/main.py
@@ -0,0 +1,33 @@
+import csv
+import numpy as np
+import plotly.express as px
+
+def getDataSource(data_path):
+    coffee = []
+    sleep = []
+    with open(data_path) as f:
+        df = csv.DictReader(f)
+        for row in df:
+            coffee.append(float(row['Coffee in ml']))
+            sleep.append(float(row['sleep in hours']))
+    return{
+        'x': coffee,
+        'y': sleep,
+    }
+
+def find_correlation(data):
+    c = np.corrcoef(data['x'], data['y'])
+    print(c[0,1])
+
+def plotFigure(data):
+    fig = px.scatter(data, x=data['x'], y=data['y'])
+    fig.show()
+
+def main():
+    data_path = 'data.csv'
+    data = getDataSource(data_path)
+    find_correlation(data)
+    plotFigure(data)
+
+if __name__ == '__main__':
+    main()
